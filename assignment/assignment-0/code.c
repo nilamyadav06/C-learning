f71d2c7010427b6ea3c1bf78806d8b5c5385e570
Nilam <nilam.ky3007@gmail.com>
2026-08-18 20:04:55 +0530

    My first C assignment

---
+++ assignment/assignment-0/code.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+int main()
+{
+    // Q1.consider int a=5,b=6 then to print on the screen is 56.
+    /*
+    int a = 5,
+        b = 6;
+    printf("%d%d", a, b);
+    */
+
+    // Q2. print on the screen is a=5 and b=6.
+    /*
+    int a = 5,
+        b = 6;
+    printf("a=%d", a);
+    printf(" and\t");
+    printf("b=%d", b);
+
+    printf("a=%d and b=%d",a,b);
+    */
+
+    // Q3. print on the screen is a=6 and b=5.
+    /*
+    int a = 6,
+        b = 5;
+    printf("a=%d", a);
+    printf(" and\t");
+    printf("b=%d", b);
+    */
+
+    // Q4.  print on the screen is value of a is 5 and value of b is 6.
+    /*
+    int a = 5, b = 6;
+    printf("Value of a is %d", a);
+    printf("\t");
+    printf("Value of b is %d", b);
+
+    printf("value of a is %d and value of b is %d", a, b);
+    */
+
+    // Q5. print on the screen is sum of 5 and 6 is 11.
+    /*
+    int a = 5, b = 6;
+    int c = a + b;
+    printf("sum of %d,%d is %d", a, b, c);
+    */
+
+    // Q6. print on the screen is 5+6=11.
+    /*
+    int a = 5, b = 6;
+    int c = a+b;
+    printf("%d + %d = %d", a, b, c);
+    */
+
+    // HWQ.1.  print on the screen is 7-3=4.
+    /*
+    int a = 7, b = 3;
+    int c = a-b;
+    printf("%d-%d=%d", a, b, c);
+    */
+
+    // HWQ.2. print on the screen enter a first number and enter a second number. find sum and print sum on screen
+    /*
+    int a, b, c;
+    printf("Enter a first number");
+    scanf("%d", &a);
+    printf("Enter a second number");
+    scanf("%d", &b);
+    c = a + b;
+    printf("%d=%d+%d", c, a, b);
+    */
+    return 0;
+}
