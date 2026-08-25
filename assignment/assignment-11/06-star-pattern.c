37dbdb7ca417327c2ca8d7a088458bfebdd1acd1
Nilam <nilam.ky3007@gmail.com>
2026-08-25 08:39:46 +0530

    Q6- completed

---
+++ assignment/assignment-11/06-star-pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+  int i, j, k;
+  for (i = 1; i <= 4; i++)
+  {
+    for (j = 1; j <= 4; j++)
+    {
+      k = i;
+      if (j <= i)
+      {
+        printf("%d", k);
+
+        k--;
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+  return 0;
+}
